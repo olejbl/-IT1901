03805c6dd723bed9ecab316e38a782ec49ec6ab3
Ingvild Løver Thon <ingvlt@stud.ntnu.no>
2019-11-11 17:48:45 +0100

    #37 center task table

--- frontend/frontend/src/components/TodoList.js
+++ frontend/frontend/src/components/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button'
 import '../CSS/TodoList.css'
+import '../CSS/TodoItems.css'
 
 //Lager todolista
 function Task( {task, deleteTask}) {
@@ -28,7 +29,7 @@ export default function TaskList (props) {
     ));
 
     return (
-      <table >
+      <table className="table">
         <thead >
         <tr>
           <th>Task</th>
